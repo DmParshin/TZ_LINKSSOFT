c2f16d949b4b0c895b5f499f7b828ac39efa29f1
Dmitry Parshin <DmParshin@gmail.com>
2021-08-04 22:54:54 +0700

    add get captcha image

--- src/main/java/com/example/tz_linkssoft/controller/CaptchaController.java
+++ src/main/java/com/example/tz_linkssoft/controller/CaptchaController.java
@@ -3,6 +3,8 @@ package com.example.tz_linkssoft.controller;
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.ShearCaptcha;
 import com.example.tz_linkssoft.captcha.Captcha;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,21 +15,21 @@ import javax.servlet.http.HttpSession;
 @RequestMapping
 public class CaptchaController {
 
-    @GetMapping("/captcha")
-    public ResponseEntity getCaptcha(HttpSession session) {
+    @RequestMapping(value = "/captcha", method = RequestMethod.GET, produces = "image/png")
+    public ResponseEntity<byte[]> getImageAsResponseEntity(HttpSession session) {
 
-        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(100, 30);
+        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(250, 100);
+        byte[] media = captcha.getImageBytes();
 
-        String captcha_code = captcha.getCode();
-        System.out.println("Captcha: " + captcha_code);
+        session.setAttribute("captcha_code", captcha.getCode());
 
-        session.getCreationTime();
-        session.setAttribute("captcha_code", captcha_code);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("request_id", session.getId());
+        headers.add("captcha_string", captcha.getCode());
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .header("request_id", session.getId())
-                .header("captcha_string", captcha_code)
-                .body(captcha.getImageBase64());
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
+        return responseEntity;
     }
 
     @PostMapping("/captcha")
@@ -35,7 +37,8 @@ public class CaptchaController {
     public ResponseEntity createProduct(@RequestBody Captcha captcha, HttpSession session) {
 
         if (session.getId().equals(captcha.getRequest_id())
-                && session.getAttribute("captcha_code").equals(captcha.getCaptcha_string())) {
+                && session.getAttribute("captcha_code").equals(captcha.getCaptcha_string())
+                && System.currentTimeMillis() - session.getCreationTime() < 300000) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body("success");
         } else return ResponseEntity.status(HttpStatus.BAD_REQUEST)
